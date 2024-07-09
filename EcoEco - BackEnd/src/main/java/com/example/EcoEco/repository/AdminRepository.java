5b4899f3e143206fa2e03bfa9fc31e9567bbda3e
BenjaminGonzalezH99 <Benjamin.Git@outlook.cl>
2024-07-09 12:44:51 -0600

    se modifico la base del proyecto, dejando la seguridad par despues

--- EcoEco - BackEnd/src/main/java/com/example/EcoEco/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoEco.repository;
-
-
-
-import com.example.EcoEco.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
